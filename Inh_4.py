d9ca42b16b0f7dca7e5a8fd5c95d3c394a52ad4c
Pranay <pranayvjadhao197@gmail.com>
2026-05-20 18:04:46 +0530

    Initial commit

---
+++ Inh_4.py
@@ -0,0 +1,11 @@
+class Base:
+    def __init__(self, x):
+        print("Inside class Base constructor, value is ", x)
+
+class Derived(Base):
+    def __init__(self):
+        super().__init__(100)
+        print("Inside class Derived constructor")
+        
+if __name__ == "__main__":
+    obj = Derived()
